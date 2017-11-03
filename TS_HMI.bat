7321a38a1c21c0d6a6eaa02f8dbfe7689bf4d55a
nbourg8 <nbourg8@gmail.com>
2017-11-03 11:01:17 -0700

    Delay for Time on HMI
    adding 3 seconds to allow time sync to HMI

--- TS_HMI.bat
+++ TS_HMI.bat
@@ -3,6 +3,7 @@ set LogFile=c:\Information.txt
 Title "%computername% : Synchronizing Times"
 
 w32tm.exe /resync
+ping 127.0.0.1 -n 3 > nul
 w32tm.exe /query /status
 
 for /f "tokens=3" %%a in ('reg query hkey_users\.default /v imageversion ^|findstr /ri "REG_SZ"') do (set image=%%a)
