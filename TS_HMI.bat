f6994ab6b680c6937b50527b296ec43f1649909f
nbourg8 <nbourg8@users.noreply.github.com>
2017-11-05 13:21:28 -0600

    Update TS_HMI.bat

--- TS_HMI.bat
+++ TS_HMI.bat
@@ -2,9 +2,10 @@
 set LogFile=c:\Information.txt
 Title "%computername% : Synchronizing Times"
 
-w32tm.exe /resync
-ping 127.0.0.1 -n 3 > nul
-w32tm.exe /query /status
+w32tm /query /peers 
+net stop w32time
+net start w32time
+w32tm /resync /nowait
 
 for /f "tokens=3" %%a in ('reg query hkey_users\.default /v imageversion ^|findstr /ri "REG_SZ"') do (set image=%%a)
 for /f "tokens=4" %%a in ('cscript slmgr.vbs /xpr ^|findstr /r "machine"') do (set activation=%%a)
