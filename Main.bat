6479ed0be8f60d7899eb4a851544e106afd951af
nbourg8 <nbourg8@users.noreply.github.com>
2017-11-05 13:11:23 -0600

    Update Main.bat

--- Main.bat
+++ Main.bat
@@ -1,26 +1,16 @@
 @echo off
 Title Collecting Information
 color 2
+net use x: /delete /y
 cls
+
 if exist log.txt del log.txt
 set LogFile="log.txt"
+
 echo.Begin>%LogFile%
+
 net use x: \\192.168.1.12\C$
 psexec \\192.168.1.12 /c TS_VM.bat %LogFile%
-copy x:\Information.txt %~dp0
-copy /b %LogFile%+Information.txt %LogFile%
-del x:\Information.txt
-net use x: /delete /y
-
-net use x: \\192.168.1.13\C$
-psexec \\192.168.1.13 /c TS_VM.bat %LogFile%
-copy x:\Information.txt %~dp0
-copy /b %LogFile%+Information.txt %LogFile%
-del x:\Information.txt
-net use x: /delete /y
-
-net use x: \\192.168.1.14\C$
-psexec \\192.168.1.14 /c TS_VM.bat %LogFile%
 copy x:\Information.txt %~dp0
 copy /b %LogFile%+Information.txt %LogFile%
 del x:\Information.txt
@@ -35,6 +25,27 @@ net use x: /delete /y
 
 net use x: \\192.168.1.23\C$
 psexec \\192.168.1.23 /c TS_VM.bat %LogFile%
+copy x:\Information.txt %~dp0
+copy /b %LogFile%+Information.txt %LogFile%
+del x:\Information.txt
+net use x: /delete /y
+
+net use x: \\192.168.1.13\C$
+psexec \\192.168.1.13 /c TS_VM.bat %LogFile%
+copy x:\Information.txt %~dp0
+copy /b %LogFile%+Information.txt %LogFile%
+del x:\Information.txt
+net use x: /delete /y
+
+net use x: \\192.168.1.20\C$
+psexec \\192.168.1.20 /c TS_VM.bat %LogFile%
+copy x:\Information.txt %~dp0
+copy /b %LogFile%+Information.txt %LogFile%
+del x:\Information.txt
+net use x: /delete /y
+
+net use x: \\192.168.1.14\C$
+psexec \\192.168.1.14 /c TS_VM.bat %LogFile%
 copy x:\Information.txt %~dp0
 copy /b %LogFile%+Information.txt %LogFile%
 del x:\Information.txt
@@ -61,14 +72,7 @@ copy /b %LogFile%+Information.txt %LogFile%
 del x:\Information.txt
 net use x: /delete /y
 
-net use x: \\192.168.1.20\C$
-psexec \\192.168.1.20 /c TS_VM.bat %LogFile%
-copy x:\Information.txt %~dp0
-copy /b %LogFile%+Information.txt %LogFile%
-del x:\Information.txt
-net use x: /delete /y
-
-REM del Information.txt
+del Information.txt
 
 Title Finished!
 pause
