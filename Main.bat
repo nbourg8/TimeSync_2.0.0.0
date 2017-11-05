8d64b6210f675b7d828797a5e0be3e7a39186f45
nbourg8 <nbourg8@users.noreply.github.com>
2017-11-05 13:40:04 -0600

    Update Main.bat

--- Main.bat
+++ Main.bat
@@ -10,63 +10,63 @@ set LogFile="log.txt"
 echo. >%LogFile%
 
 net use x: \\192.168.1.12\C$
-psexec \\192.168.1.12 /c TS_VM.bat %LogFile%
+psexec \\192.168.1.12 -c -f TS_VM.bat %LogFile%
 copy x:\Information.txt %~dp0
 copy /b %LogFile%+Information.txt %LogFile%
 del x:\Information.txt
 net use x: /delete /y
 
 net use x: \\192.168.1.16\C$
-psexec \\192.168.1.16 /c TS_VM.bat %LogFile%
+psexec \\192.168.1.16 -c -f TS_VM.bat %LogFile%
 copy x:\Information.txt %~dp0
 copy /b %LogFile%+Information.txt %LogFile%
 del x:\Information.txt
 net use x: /delete /y
 
 net use x: \\192.168.1.23\C$
-psexec \\192.168.1.23 /c TS_VM.bat %LogFile%
+psexec \\192.168.1.23 -c -f TS_VM.bat %LogFile%
 copy x:\Information.txt %~dp0
 copy /b %LogFile%+Information.txt %LogFile%
 del x:\Information.txt
 net use x: /delete /y
 
 net use x: \\192.168.1.13\C$
-psexec \\192.168.1.13 /c TS_VM.bat %LogFile%
+psexec \\192.168.1.13 -c -f TS_VM.bat %LogFile%
 copy x:\Information.txt %~dp0
 copy /b %LogFile%+Information.txt %LogFile%
 del x:\Information.txt
 net use x: /delete /y
 
 net use x: \\192.168.1.20\C$
-psexec \\192.168.1.20 /c TS_VM.bat %LogFile%
+psexec \\192.168.1.20 -c -f TS_VM.bat %LogFile%
 copy x:\Information.txt %~dp0
 copy /b %LogFile%+Information.txt %LogFile%
 del x:\Information.txt
 net use x: /delete /y
 
 net use x: \\192.168.1.14\C$
-psexec \\192.168.1.14 /c TS_VM.bat %LogFile%
+psexec \\192.168.1.14 -c -f TS_VM.bat %LogFile%
 copy x:\Information.txt %~dp0
 copy /b %LogFile%+Information.txt %LogFile%
 del x:\Information.txt
 net use x: /delete /y
 
 net use x: \\192.168.1.180\C$
-psexec \\192.168.1.180 /c TS_HMI.bat %LogFile%
+psexec \\192.168.1.180 -c -f TS_HMI.bat %LogFile%
 copy x:\Information.txt %~dp0
 copy /b %LogFile%+Information.txt %LogFile%
 del x:\Information.txt
 net use x: /delete /y
 
 net use x: \\192.168.1.181\C$
-psexec \\192.168.1.181 /c TS_HMI.bat %LogFile%
+psexec \\192.168.1.181 -c -f TS_HMI.bat %LogFile%
 copy x:\Information.txt %~dp0
 copy /b %LogFile%+Information.txt %LogFile%
 del x:\Information.txt
 net use x: /delete /y
 
 net use x: \\192.168.1.5\C$
-psexec \\192.168.1.5 /c TS_HMI.bat %LogFile%
+psexec \\192.168.1.5 -c -f TS_HMI.bat %LogFile%
 copy x:\Information.txt %~dp0
 copy /b %LogFile%+Information.txt %LogFile%
 del x:\Information.txt
