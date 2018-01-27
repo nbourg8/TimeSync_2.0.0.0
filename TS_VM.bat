506c92b4285873c00ef09aa86cfc0661634c12eb
nbourg8 <nbourg8@users.noreply.github.com>
2018-01-27 11:45:42 -0600

    Update TS_VM.bat
    Updating log.txt to easier read and copy/paste action

--- TS_VM.bat
+++ TS_VM.bat
@@ -9,5 +9,4 @@ vmwaretoolboxcmd.exe timesync enable
 
 for /f "tokens=3" %%a in ('reg query hkey_users\.default /v imageversion ^|findstr /ri "REG_SZ"') do (set image=%%a)
 for /f "tokens=4" %%a in ('cscript slmgr.vbs /xpr ^|findstr /r "machine"') do (set activation=%%a)
-echo.%computername%,%image%,%activation%,%date%,%time%>%LogFile%
-
+echo.%date% %time% %activation% %image% %computername%>%LogFile%
