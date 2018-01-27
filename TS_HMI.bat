696db9316e19d696aebbd89d640c4c0c8b63a827
nbourg8 <nbourg8@users.noreply.github.com>
2018-01-27 11:43:05 -0600

    Update TS_HMI.bat
    Adjusting layout of Log.txt for easier reading and copy/paste action

--- TS_HMI.bat
+++ TS_HMI.bat
@@ -8,4 +8,4 @@ w32tm /resync /nowait
 
 for /f "tokens=3" %%a in ('reg query hkey_users\.default /v imageversion ^|findstr /ri "REG_SZ"') do (set image=%%a)
 for /f "tokens=4" %%a in ('cscript slmgr.vbs /xpr ^|findstr /r "machine"') do (set activation=%%a)
-echo.%computername%,%image%,%activation%,%date%,%time%>%LogFile%
+echo.%date% %time% %activation% %image% %computername%>%LogFile%
